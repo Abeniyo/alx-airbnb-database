d0b4f65284f60e4a60c4f194d74de1af04de0473
Abeniyo <avelox6867@gmail.com>
2025-05-13 22:24:37 +0300

    This is the first INNER JOIN answer

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,16 @@
+-- 1. INNER JOIN: this is retrieve all bookings and the respective users who made those bookings
+SELECT
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM
+    Booking b
+INNER JOIN
+    User u ON b.user_id = u.user_id;
