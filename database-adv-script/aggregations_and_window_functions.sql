6218c05ecbcdf675118a55f6a9b450387677c5dd
Abeniyo <avelox6867@gmail.com>
2025-05-18 13:23:46 +0300

    The updated part of joins_queries.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -17,12 +17,11 @@ ORDER BY
 
 -- 2. WINDOW FUNCTION QUERY:
 -- Rank properties based on the total number of bookings
-
 SELECT
     p.property_id,
     p.name,
     COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
 FROM
     Property p
 LEFT JOIN
