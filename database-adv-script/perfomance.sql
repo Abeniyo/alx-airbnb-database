345a92132aa3a2d07eff05622191e3c5c7e5cc20
Abeniyo <avelox6867@gmail.com>
2025-05-18 13:37:30 +0300

    this update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,3 +1,5 @@
+-- Initial Complex Query (Before Optimization)
+EXPLAIN
 SELECT
     b.booking_id,
     b.start_date,
@@ -18,4 +20,24 @@ SELECT
 FROM Booking b
 JOIN User u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed'
+  AND b.start_date >= '2025-01-01';
+
+
+-- Optimized Query (After Indexing and Field Reduction)
+EXPLAIN
+SELECT
+    b.booking_id,
+    b.start_date,
+    b.total_price,
+    u.first_name,
+    u.last_name,
+    p.name AS property_name,
+    pay.amount
+FROM Booking b
+JOIN User u ON b.user_id = u.user_id
+JOIN Property p ON b.property_id = p.property_id
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed'
+  AND b.start_date >= '2025-01-01';
